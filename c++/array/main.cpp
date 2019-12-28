4844d6ec6ebac63357b38a2a6b03339474227e23
iJinxin <874450097@qq.com>
2019-12-28 16:12:51 +0800

    array test

---
+++ c++/array/main.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int i = 0;
+  int arr[4] = {0};
+  for (; i <= 4; i++) {
+    arr[i] = 0;
+    cout << "hello world" << endl;
+  }
+  return 0;
+}
