81f9d578321713a5bc27f0ffe5b31323c5b9a509
iJinxin <874450097@qq.com>
2020-01-13 15:21:02 +0800

    selectionSort

--- sort/array_sort.cpp
+++ sort/array_sort.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+// 冒泡排序
 void bubbleSort(int arr[], int size) {
   clock_t start_t, end_t;
   start_t = clock();
@@ -26,6 +27,7 @@ void bubbleSort(int arr[], int size) {
        << "ms" << endl;
 }
 
+// 插入排序
 void insertionSort(int arr[], int size) {
   if (size <= 1) return;
   clock_t start_t, end_t;
@@ -47,7 +49,28 @@ void insertionSort(int arr[], int size) {
        << "ms" << endl;
 }
 
-void selectSort() {}
+// 选择排序
+void selectionSort(int arr[], int size) {
+  if (size <= 1) return;
+  clock_t start_t, end_t;
+  start_t = clock();
+  for (int i = 0; i < size; ++i) {
+    int minIndex = i;
+    for (int j = i + 1; j < size; ++j) {
+      if (arr[j] < arr[i]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex != i) {
+      int temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  end_t = clock();
+  cout << "selectionSort " << size << ", use time: " << double(end_t - start_t)
+       << "ms" << endl;
+}
 
 int main() {
   int arr[RANDOM_SIZE] = {0};
@@ -62,5 +85,12 @@ int main() {
   int arr2[RANDOM_SIZE];
   copy(begin(arr), end(arr), begin(arr2));
   insertionSort(arr2, RANDOM_SIZE);
+
+  // select
+  int arr3[RANDOM_SIZE];
+  copy(begin(arr), end(arr), begin(arr3));
+  selectionSort(arr3, RANDOM_SIZE);
+
+
   return 0;
 }
