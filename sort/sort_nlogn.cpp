b31f60fa1056890a321ae1b0c029db5d5b17a2e4
iJinxin <874450097@qq.com>
2020-01-14 16:50:57 +0800

    quicksort

--- sort/sort_nlogn.cpp
+++ sort/sort_nlogn.cpp
@@ -1,7 +1,8 @@
 #include <time.h>
+#include <cstdlib>
 #include <iostream>
 
-#define RANDOM_SIZE 100  // 排序量级
+#define RANDOM_SIZE 100000  // 排序量级
 
 using namespace std;
 
@@ -44,11 +45,36 @@ void merge_sort(int arr[], int p, int r) {
   merge(arr, p, q, r);
 }
 
+// 数组拆分，返回pivot的位置
+int partition(int arr[], int p, int r) {
+  int pivot = arr[r];
+  int i = p;
+  for (int j = p; j < r; j++) {
+    if (arr[j] < pivot) {
+      if (i!=j) {
+        swap(arr[i], arr[j]);
+      }
+      i++;
+    }
+  }
+  swap(arr[i], arr[r]);
+
+  return i;
+}
+
+void quicksort(int arr[], int p, int r) {
+  if (p >= r) return;
+  int q = partition(arr, p, r);
+  quicksort(arr, p, q - 1);
+  quicksort(arr, q+1, r);
+}
+
 int main() {
   int arr[RANDOM_SIZE] = {0};
   for (int i = 0; i < RANDOM_SIZE; i++) {
     arr[i] = rand() % 1000;
   }
+
   // merge sort
   int arr1[RANDOM_SIZE];
   copy(begin(arr), end(arr), begin(arr1));
@@ -59,7 +85,15 @@ int main() {
   cout << "merge sort " << RANDOM_SIZE << ", use time: " << (end_t - start_t)
        << "ms" << endl;
 
-
+  // quicksort
+  int arr2[RANDOM_SIZE];
+  copy(begin(arr), end(arr), begin(arr2));
+  clock_t start_t2, end_t2;
+  start_t2 = clock();
+  quicksort(arr2, 0, RANDOM_SIZE - 1);
+  end_t2 = clock();
+  cout << "quicksort " << RANDOM_SIZE << ", use time: " << (end_t2 - start_t2)
+       << "ms" << endl;
   // for (int i=0;i<RANDOM_SIZE;i++) {
   //   cout << arr1[i] << endl;
   // }
